2e203f9ff1427d44113772fac5a424a52f32495c
AlphaHydrae <hydrae.alpha@gmail.com>
2011-10-02 12:36:50 +0200

    Completed commands specs.

--- spec/commands/show_spec.rb
+++ spec/commands/show_spec.rb
@@ -16,4 +16,10 @@ describe Scide::Commands::Show do
     com = Scide::Commands::Show.new 'fubar'
     com.to_screen.should == 'stuff "fubar"'
   end
+
+  it "should use #text_with_options as argument to stuff" do
+    com = Scide::Commands::Show.new 'fubar'
+    text_with_options = com.text_with_options
+    com.to_screen.should == %|stuff "#{text_with_options}"|
+  end
 end
